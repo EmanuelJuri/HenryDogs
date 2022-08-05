61ad86d1c44d0b2b8f552279eb1a4617f11a5329
Emanuel Juri <emanueljuri@gmail.com>
2022-08-05 13:18:02 -0300

    changes landingPage

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 import './LandingPage.css';
+import dogPictutre from '../../images/perrolanding.svg';
 
 export default function LandingPage(){
     return(
         <div className='background'>
             <div className='container_ld'>
-                <h1 className='welcome_title'>Del primero al último{/* , todos los perros del mundo */}</h1>
-                <Link to='/home'>
-                    <button className='button_intro'>INGRESAR</button>
-                </Link>
-                <h1 className='welcome_title'>Todos los perros del mundo</h1>
+                <h1 className='welcome_title'>Del primero al último! Todos los perros del mundo</h1>
+                <h2 className='welcome_subtitle'>Que estas esperando para entrar...</h2>
+            <Link to='/home'>
+                <button className='button_intro'>INGRESAR</button>
+            </Link>                
+            </div>
+            <div>
+                <img className='image_ld' src={dogPictutre} alt="dogPicture" />
             </div>
         </div>
     )
