7597414f40708ad9c25495631fb4995cf70627e6
Emanuel Juri <emanueljuri@gmail.com>
2022-08-07 17:52:59 -0300

    changes detail

--- client/src/components/DogDetail/DogDetail.jsx
+++ client/src/components/DogDetail/DogDetail.jsx
@@ -5,6 +5,9 @@ import { useEffect } from 'react';
 
 import { getDetail, cleanDogId } from '../../actions/index';
 import loading from '../../images/loading-load.gif'
+import height from  '../../images/perroHeight.svg'
+import weight from  '../../images/scale-svgrepo.svg'
+import lifeSpan from  '../../images/birthday-cake.svg'
 
 import './DogDetail.css'
 
@@ -19,36 +22,53 @@ export default function Detail(props){
     },[dispatch, props.match.params.id])
 
     const myDog = useSelector(state => state.detail)
-
-    // console.log(myDog, 'myDog')
-
+    // const myDogname = useSelector(state => state.detail)
+    // console.log('#######', myDogname)
+    console.log('myDog ESTADO',myDog)
+    console.log('name', myDog.name &&myDog.name.length)
     return(
         <div className='background_detail'>
-            <div className='cardContainer'>                            
+            <div /* className='cardContainer' */>                            
                 {
                 myDog ?
-                <div>
-                    <div  key={myDog.id} >                    
-                        <h1 className='d_name'>{myDog.name}</h1>
+                <div className='cardContainer'>
+                    <div
+                        className='left'
+                        key={myDog.id} >                    
+                        <h1 className= {myDog.name&&myDog.name.length > 19 ? 'd_nameLong' : 'd_name'}>{myDog.name}</h1>                        
                         <img className='detail_image' src={myDog.image} alt="img not found" />
                     </div>
 
                     <div className="description">
-                        <h3>Altura: {myDog.height} centímetros</h3> 
-                        <h3>Peso: {myDog.weight} kilogramos</h3> 
-                        <h3>Esperanza de vida: {myDog.life_span} años</h3>                    
-                        <h3>Temperamentos:</h3><h4>{myDog.temperament}</h4> 
-                    </div>
+                        <div className='descriptionDetail'>
+                            <img src={height} alt="height-dog" />
+                            <h3 className='dataDetail'>Altura: {myDog.height} centímetros</h3> 
+                        </div>
 
-                    <div>
-                        <Link to= '/home'>
-                                <button className='button_home'>Volver</button>
-                        </Link>
+                        <div className='descriptionDetail'>
+                            <img src={weight} alt="weight-dog" />
+                            <h3 className='dataDetail'>Peso: {myDog.weight} kilogramos</h3> 
+                        </div>
+
+                        <div className='descriptionDetail'>
+                            <img src={lifeSpan} alt="lifeSpan-dog" />
+                            <h3 className='dataDetail'>Esperanza de vida: {myDog.life_span} años</h3>
+                        </div>
+
+                        <div className='temperamentDetail'>
+                            <h3 className='temperamentTittle'>Temperamentos:</h3><h4>{myDog.temperament}</h4>
+                        </div>
+                        
                     </div>
                 </div>
                 :                
                 <img src={loading} alt="cargando..." className="carga"/>
                 }
+            </div>            
+            <div>
+                <Link to= '/home'>
+                        <button className='button_home'>Volver</button>
+                </Link>
             </div>
         </div>
     )
